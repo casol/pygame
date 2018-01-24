11bbf22b048a963f4faf07bfa78d292a962b8db7
casol <frombitstobytes.com@gmail.com>
2018-01-24 19:37:31 +0100

    Add gravity

--- test_pygame.py
+++ test_pygame.py
@@ -12,9 +12,21 @@ WHITE = (255, 255, 255)
 (width, height) = (400, 400)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Hello World!')
+DRAG = 0.999
+ELASTICITY = 0.75
+GRAVITY = (math.pi, 0.002)
 
 
-# object
+def add_vectors(angle1, length1, angle2, length2):
+    x = math.sin(angle1) * length1 + math.sin(angle2) * length2
+    y = math.cos(angle1) * length1 + math.cos(angle2) * length2
+
+    angle = 0.5 * math.pi - math.atan2(y, x)
+    length = math.hypot(x, y)
+
+    return angle, length
+
+
 class Particle:
     """Creates a particle object."""
     def __init__(self, x_y, size):
@@ -22,7 +34,7 @@ class Particle:
         self.size = size
         self.colour = (0, 0, 255)
         self.thickness = 2
-        self.speed = 0.01
+        self.speed = 0
         self.angle = math.pi/2
 
     def display(self):
@@ -32,26 +44,33 @@ class Particle:
 
     def move_particle(self):
         """Move particle."""
+        (self.angle, self.speed) = add_vectors(self.angle, self.speed, math.pi, 0.002)
         self.x += math.sin(self.angle) * self.speed
         self.y -= math.cos(self.angle) * self.speed
+        self.speed *= DRAG
 
     def bounce(self):
         """Bounce particle form the walls."""
         if self.x >= width - self.size:
             self.x = 2*(width - self.size) - self.x
             self.angle = - self.angle
+            self.speed *= ELASTICITY
 
         elif self.x <= self.size:
             self.x = 2*self.size - self.x
             self.angle = - self.angle
+            self.speed *= ELASTICITY
 
         if self.y > height - self.size:
             self.y = 2*(height - self.size) - self.y
             self.angle = math.pi - self.angle
+            self.speed *= ELASTICITY
 
         elif self.y < self.size:
             self.y = 2*self.size - self.y
             self.angle = math.pi - self.angle
+            self.speed *= ELASTICITY
+
 
 number_of_particle = 10
 particles = []
@@ -60,10 +79,10 @@ particles = []
 for n in range(number_of_particle):
     size = random.randint(10, 20)
     x = random.randint(size, width - size)
-    y = random.randint(size, height - size)
+    y = 20
     particle = (Particle((x, y), size))
     # update speed and angle of existing object
-    particle.speed = 0.01
+    particle.speed = random.random()
     particle.angle = random.uniform(0, math.pi*2)
     # add new particle to the list
     particles.append(particle)
