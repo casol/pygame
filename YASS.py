d47df314c31e48dcce2dca467ea6439150037755
casol <frombitstobytes.com@gmail.com>
2018-02-04 21:14:22 +0100

    Add missile and asteroid class, update with sprite collide method

--- YASS.py
+++ YASS.py
@@ -11,7 +11,6 @@ from pygame.locals import *  # pygame.locals.QUIT --> QUIT
 game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'img')
 sound_folder = os.path.join(game_folder, 'sound')
-player_img = pygame.image.load(os.path.join(img_folder, 'ship.png'))
 
 # initialize pygame
 pygame.init()
@@ -21,7 +20,7 @@ fps_clock = pygame.time.Clock()
 
 # set up the window
 WIDTH = 800
-HEIGHT = 600
+HEIGHT = 800
 DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('YESS - Yet_Another_Space_Shooter')
 
@@ -34,35 +33,119 @@ BLACK = (0, 0, 0)
 
 
 class Player(pygame.sprite.Sprite):
-    """Create sprite."""
+    """The player's ship."""
     def __init__(self):
+        """Initialise player object."""
         # Call the parent class (Sprite constructor)
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
+        # set ship image
+        self.image = player_img
+        # resize original ship image
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2, HEIGHT/2)
-        self.speed_x = 0
-        self.speed_y = 0
-        self.x = 0
-        self.y = 0
+        self.delta_x = 0
+        self.delta_y = 0
 
     def update(self):
-        """Rotate based on keys pressed."""
+        """Move based on keys pressed."""
+        self.delta_x = 5
+        self.delta_y = 5
         key = pygame.key.get_pressed()
-        distance = 5  # distance moved in 1 frame
         if key[K_DOWN]:  # down key
-            self.rect.y += distance  # move down
+            self.rect.y += self.delta_y  # move down
         elif key[K_UP]:  # up key
-            self.rect.y -= distance  # move up
+            self.rect.y -= self.delta_y  # move up
         if key[K_RIGHT]:  # right key
-            self.rect.x += distance  # move right
+            self.rect.x += self.delta_x  # move right
         elif key[K_LEFT]:  # left key
-            self.rect.x -= distance  # move left
+            self.rect.x -= self.delta_x  # move left
+        if key[K_SPACE]:
+            player.shoot()
+
+    def wrap_around_screen(self):
+        """Wrap around screen."""
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+        if self.rect.right < 0:
+            self.rect.left = WIDTH
+        if self.rect.bottom <= 0:
+            self.rect.top = HEIGHT
+        if self.rect.top > HEIGHT:
+            self.rect.bottom = 0
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+    def shoot(self):
+        missile = Missile(self.rect.centerx, self.rect.top)
+        all_sprites.add(missile)
+        missiles.add(missile)
+
+
+class Missile(pygame.sprite.Sprite):
+    """A missile launched by the player's ship."""
+    def __init__(self, x, y):
+        """Initialize missile sprite."""
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 15))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        # missile going upward
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # out of the screen
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+class Asteroid(pygame.sprite.Sprite):
+    """Asteroid object."""
+    def __init__(self):
+        """Initialize asteroid sprite."""
+        pygame.sprite.Sprite.__init__(self)
+        self.image = asto_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+
+# load all games graphic
+background = pygame.image.load(os.path.join(img_folder, 'darkPurple.png')).convert()
+background_rect = background.get_rect()
+background = pygame.transform.scale(background, (800, 800))
+player_img = pygame.image.load(os.path.join(img_folder, 'playerShip1_red.png')).convert()
+asto_img = pygame.image.load(os.path.join(img_folder, 'meteorBrown_med1.png')).convert()
+
 
 all_sprites = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+missiles = pygame.sprite.Group()
+
 player = Player()
-all_sprites.add(player)
+#all_sprites.add(player)
+
+for i in range(8):
+    d = Asteroid()
+    all_sprites.add(d)
+    asteroids.add(d)
+
 
 while True:  # game loop
     for event in pygame.event.get():
@@ -71,8 +154,29 @@ while True:  # game loop
             sys.exit()
 
     DISPLAY.fill(BLACK)
+    DISPLAY.blit(background, background_rect)
     # drew / update
     all_sprites.update()
     all_sprites.draw(DISPLAY)
+    player.draw(DISPLAY)
+    player.update()
+    player.wrap_around_screen()
+    # check if a missile hit an asteroid
+    hits = pygame.sprite.groupcollide(asteroids, missiles, True, True)
+    # create new
+    for hit in hits:
+        d = Asteroid()
+        all_sprites.add(d)
+        asteroids.add(d)
+
+    # check if a asteroid the the ship
+    hits = pygame.sprite.spritecollide(player, asteroids, False)
+    if hits:
+        pygame.quit()
+        sys.exit()
+
+    print(player.rect.x, player.rect.y, 'top:', player.rect.top, 'bottom:', player.rect.bottom,
+          'rect_x:', player.rect.x, 'rect_y:', player.rect.y, player.rect.centerx)
+
     pygame.display.update()
     fps_clock.tick(FPS)
--- test_field.py
+++ test_field.py
@@ -1,113 +1,68 @@
-# Yet Another Space Shooter
-# https://github.com/casol/pygame
+""" rotate.py
+    demonstrate rotating a sprite"""
 
 import pygame
-import sys
-import random
-from pygame.locals import *  # pygame.locals.QUIT --> QUIT
+import os
 
 pygame.init()
 
-FPS = 30  # frames per second
-fps_clock = pygame.time.Clock()
-
-# set up the window
-WIDTH = 800
-HEIGHT = 600
-DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('YESS - Yet_Another_Space_Shooter')
-
-# RGB colors
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-x = 200
-y = 300
-distance = 5
-angle = 10
-
-
-# subclassing the Sprite
-class Block(pygame.sprite.Sprite):
-    """Create sprite."""
-    def __init__(self, color, width, height, x, y):
-        # Call the parent class (Sprite constructor)
-        super(Block, self).__init__()
-        #pygame.sprite.Sprite.__init__(self)
-        self.x = x
-        self.y = y
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
-        # Fetch the rectangle object that has the dimensions of the image
-        # Update the position of this object by setting the values of rect.x and rect.y
-        self.rect = self.image.get_rect()
-
-    def draw(self, surface):
-        """Draw on surface."""
-        # blit yourself at your current position
-        surface.blit(self.image, (self.x, self.y))
-
 
 class Ship(pygame.sprite.Sprite):
-    """Create ship sprite."""
     def __init__(self):
-        # Call the parent class (Sprite constructor)
         pygame.sprite.Sprite.__init__(self)
-        self.x = WIDTH/2
-        self.y = HEIGHT/2
-        # Create an image of the ship
-        self.ship_image = pygame.image.load('ship.png')
-        self.rect = self.ship_image.get_rect()
+        self.imageMaster = pygame.image.load("ship.png")
+        self.imageMaster = self.imageMaster.convert()
+        self.image = self.imageMaster
+        self.rect = self.image.get_rect()
+        self.rect.center = (320, 240)
+        self.dir = 0
 
-    def draw(self, surface):
-        """Draw on surface."""
-        # blit yourself at your current position
-        surface.blit(self.ship_image, (self.x, self.y))
+    def update(self):
+        oldCenter = self.rect.center
+        self.image = pygame.transform.rotate(self.imageMaster, self.dir)
+        self.rect = self.image.get_rect()
+        self.rect.center = oldCenter
 
-    def handle_keys(self):
-        """Rotate based on keys pressed."""
-        key = pygame.key.get_pressed()
-        distance = 5  # distance moved in 1 frame
-        if key[K_DOWN]:  # down key
-            self.y += distance  # move down
-        elif key[K_UP]:  # up key
-            self.y -= distance  # move up
-        if key[K_RIGHT]:  # right key
-            self.x += distance  # move right
-        elif key[K_LEFT]:  # left key
-            self.x -= distance  # move left
+    def turnLeft(self):
+        self.dir += 5
+        if self.dir > 360:
+            self.dir = 45
 
-# block group
-block_obj = Block(RED, 20, 30, 600, 500)
-blocks = pygame.sprite.Group()
-blocks.add(block_obj)
-
-# player group
-ship_obj = Ship()
-player_group = pygame.sprite.Group()
-player_group.add(ship_obj)
-
-all_sprites = pygame.sprite.Group()
+    def turnRight(self):
+        self.dir -= 5
+        if self.dir < 0:
+            self.dir = 315
 
 
+def main():
+    screen = pygame.display.set_mode((640, 480))
+    pygame.display.set_caption("Rotate a sprite")
 
-while True:  # game loop
-    DISPLAY.fill(WHITE)
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+    background = pygame.Surface(screen.get_size())
+    background.fill((0, 0, 0))
 
-    # meteor_image = pygame.image.load('meteor.png')
-    all_sprites.update()
-    all_sprites.draw(DISPLAY)
-    ship_obj.handle_keys()
-    ship_obj.draw(DISPLAY)
-    block_obj.draw(DISPLAY)
-    pygame.display.update()
-    fps_clock.tick(FPS)
+    ship = Ship()
+    allSprites = pygame.sprite.Group(ship)
+
+    clock = pygame.time.Clock()
+    keepGoing = True
+    while keepGoing:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    ship.turnLeft()
+                elif event.key == pygame.K_RIGHT:
+                    ship.turnRight()
+
+        allSprites.clear(screen, background)
+        allSprites.update()
+        allSprites.draw(screen)
+
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
